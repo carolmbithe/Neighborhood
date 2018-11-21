369186de361fc982cb81529d534dec733d613e07
carolmbithe <carombithe97@gmail.com>
2018-11-21 17:02:32 +0300

    First render

--- updates/views.py
+++ updates/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from django.core.exceptions import ObjectDoesNotExist
+# from .models import  Project
+
+
+
 
 # Create your views here.
+# @login_required(login_url='/accounts/login/')
+def index(request):
+
+    return render(request,'index.html')
