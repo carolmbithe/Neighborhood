c152188a28d549c592ee4cea5b9294337e7c5b8d
carolmbithe <carombithe97@gmail.com>
2018-11-26 08:30:58 +0300

    heroku deployment

--- updates/tests.py
+++ updates/tests.py
@@ -23,37 +23,18 @@ class NeighborHoodTestClass(TestCase):
         self.new_neighborhood.delete_neighborhood()
 
 
-    # def find_neighborhood_by_id(self):
-    #     neighborhood=NeighborHood.find_neighborhood_by_id()
-    #     self.assertTrue(len(neighborhood)>0)
+    def test_find_neighborhood_by_id(self):
+        neighborhood=NeighborHood.find_neighborhood_by_id()
+        self.assertTrue(len(neighborhood)>0)
 
     def test_update_occupants(self):
         self.new_neighborhood.save_neighborhood()
         self.new_neighborhood = NeighborHood.objects.filter(id=1)
         neighborhood = self.new_neighborhood
-        neighborhood.update_occupants(40)
+        Neighborhood.update_occupants(40)
         self.updated_neighborhood = NeighborHood.objects.get(id=1)
         self.assertEqual(self.updated_neighborhood.occupants_count,40)
 
-
-class UserprofileTestClass(TestCase):
-    #Set up method
-    def setUp(self):
-        self.new_userprofile =Userprofile(profile_image="image.jpeg",email="profile@gmail.com")
-
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_userprofile,Userprofile))
-
-    #Testing Save Method
-    def test_save_method(self):
-        self.new_userprofile.save_userprofile()
-        userprofiles=Userprofile.objects.all()
-        self.assertTrue(len(userprofiles)>0)
-
-    def test_delete_method(self):
-        self.new_userprofile.save_userprofile()
-        self.new_userprofile.delete_userprofile()
 
 
 
@@ -81,6 +62,28 @@ class BuinessTestClass(TestCase):
     def test_find_business_by_id(cls,business_id):
         business=Business.find_business_by_id()
         self.assertTrue(len(business)>0)
+
+
+
+class UserprofileTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.new_userprofile =Userprofile(profile_image="image.jpeg",email="profile@gmail.com")
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_userprofile,Userprofile))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.new_userprofile.save_userprofile()
+        userprofiles=Userprofile.objects.all()
+        self.assertTrue(len(userprofiles)>0)
+
+    def test_delete_method(self):
+        self.new_userprofile.save_userprofile()
+        self.new_userprofile.delete_userprofile()
+
 
 
 class PostTestClass(TestCase):
